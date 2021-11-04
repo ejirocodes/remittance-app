394ecdfff1dd8d2fdae9e46fb7dab95f74e71a8a
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-11-04 16:04:05 +0100

    bare workflow init [added tailwind ]

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="div">
+      <input type="text" placeholder="Where are you sending money to?" class="" />
+      <button>Check rate</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
